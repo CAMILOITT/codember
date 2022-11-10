a379c3fd5457df62af6638c48488d1d9e164b313
CAMILOITT <camilocamilo5@outlook.com>
2022-11-10 17:03:41 -0500

    add lint and formatter

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: ['standard','eslint-config-prettier'],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+  }
+}
