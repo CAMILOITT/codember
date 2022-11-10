9b72d497465c9570a344ba2a1c6b165abb3fb53c
CAMILOITT <camilocamilo5@outlook.com>
2022-11-10 00:52:56 -0500

    add solution challeng js

---
+++ challenge01/js/main.js
@@ -0,0 +1,35 @@
+async function main() {
+  const response = await fetch('https://codember.dev/users.txt');
+  const data = await response.text();
+  const separar = data.split('\n');
+  let agrupar = [];
+  const result = [];
+  separar.forEach((item) => {
+    if (item !== '') {
+      agrupar.push(item.split(' '));
+    } else {
+      result.push(agrupar.flat());
+      agrupar = [];
+    }
+  });
+  const union = [];
+  for (let i = 0; i < result.length; i++) {
+    const element = result[i];
+    element.forEach((item) => {
+      agrupar.push(item.split(':'));
+    });
+    union.push(Object.fromEntries(agrupar));
+    agrupar = [];
+  }
+  let count = 0;
+  const res = union.map((user) => {
+    if (user.usr && user.psw && user.eme && user.age && user.loc && user.fll) {
+      count++;
+      return user;
+    }
+  });
+  console.log(res);
+  console.log(count);
+  console.log(res[count]);
+}
+main();
